003a6ad19f5d2fdf76f90f1e1bd54df28662a176
mchwalisz <mchwalisz@ed387708-e0b6-498a-9351-8cf83ab2cca3>
2011-10-24 15:22:12 +0000

    crewcdf: add unifyTime to force common starting time for all measurements in cell array
    Signed-off-by: Mikolaj Chwalisz <chwalisz@tkn.tu-berlin.de>
    
    git-svn-id: https://svn.atlantis.ugent.be/svn/CREW@143 ed387708-e0b6-498a-9351-8cf83ab2cca3

--- crewcdf_unifyTime.m
+++ crewcdf_unifyTime.m
@@ -5,3 +5,40 @@ function [ p ] = crewcdf_unifyTime(p, varargin)
 %
 
 %   Mikolaj Chwalisz for CREW
+
+iP = inputParser;
+iP.addRequired('p');
+iP.addOptional('Tstart','',@(x)isdate(x,'dd-mmm-yyyy HH:MM:SS'));
+iP.parse(p, varargin{:});
+options = iP.Results;
+if isempty(options.Tstart)
+    apstru = [p{:}]';
+    options.Tstart = datestr(min(datenum({apstru.Tstart})));
+end
+
+for ii=1:length(p)
+    [p{ii}.Tstart, p{ii}.SampleTime ] = TimeConvRel( ...
+        options.Tstart, p{ii}.Tstart, p{ii}.SampleTime );
+end
+end
+
+function result = isdate(dateString,dateformat)
+try
+    datenum(dateString,dateformat);
+    result = true;
+catch
+    result = false;
+end
+end
+
+function [ tStart, tVector ] = TimeConvRel( tStartNew, tStartOrg, tVectorOrg )
+%TIMECONVREL Convert relative time format to new starting time
+%   [ tStart, tVector ] = TimeConvRel( tStartNew, tStartOrg, tVectorOrg )
+%
+%   Mikolaj Chwalisz for CREW in Sept 2011
+tStart = tStartNew;
+tDiff = datenum(tStartNew) - datenum(tStartOrg);
+tDiffSec = 24*60*60*tDiff;
+tVector = tVectorOrg - tDiffSec;
+end
+
