9111211fe95430d457a7ff8400fcc7991d6d7baa
mchwalisz <mchwalisz@ed387708-e0b6-498a-9351-8cf83ab2cca3>
2011-10-28 08:46:08 +0000

    crewcdf: loaddir now can output also file names
    git-svn-id: https://svn.atlantis.ugent.be/svn/CREW@147 ed387708-e0b6-498a-9351-8cf83ab2cca3

--- crewcdf_loaddir.m
+++ crewcdf_loaddir.m
@@ -1,4 +1,4 @@
-function [ p ] = crewcdf_loaddir(dirNames)
+function [ p , pFileName ] = crewcdf_loaddir(dirNames)
 %CREWCDF_LOADDIR Loads all files from dirNames directories.
 %   CREWCDF_LOAD(dirNames)  Tries to load all files in diven directories.
 %
@@ -9,23 +9,30 @@ function [ p ] = crewcdf_loaddir(dirNames)
 % Mikolaj Chwalisz for CREW
 
 pAll{1} = 0;
+pFileName{1} = '';
 for ii=1:length(dirNames)
-files = dir([dirNames{ii} filesep '*']);
-pdir = cell(size(files,1),1);
-errno = 0;
-for i2=1:size(files,1)
-   fileName = files(i2).name;
-   disp(fileName)
-   try
-   p = crewcdf_load([dirNames{ii} filesep fileName]);
-   pdir{i2-errno} = p;
-   catch ME
-       pdir(i2-errno) = [];
-       errno = errno + 1;
-       clear ME;
-   end
-end
-pAll = {pAll{:}, pdir{:}};
+    files = dir([dirNames{ii} filesep '*']);
+    pdir = cell(size(files,1),1);
+    pFileNamedir = cell(size(files,1),1);
+    errno = 0;
+    for i2=1:size(files,1)
+        fileName = files(i2).name;
+        disp(fileName)
+        try
+            p = crewcdf_load([dirNames{ii} filesep fileName]);
+            pdir{i2-errno} = p;
+            pFileNamedir{i2 - errno} = fileName;
+        catch ME
+            pdir(i2-errno) = [];
+            pFileNamedir(i2 - errno) = [];
+            errno = errno + 1;
+            clear ME;
+        end
+    end
+    pAll = {pAll{:}, pdir{:}};
+    pFileName = {pFileName{:}, pFileNamedir{:}};
 end
 pAll(1) = [];
+pFileName(1) = [];
+pFileName = pFileName';
 p = pAll';
