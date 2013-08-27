a0405ab80feb11c71beac5aa1c12d429973343ef
Mikolaj Chwalisz <chwalisz@tkn.tu-berlin.de>
2013-08-27 10:54:47 +0200

    init Contents.m

---
+++ Contents.m
@@ -0,0 +1,22 @@
+% CREWCDF_TOOLBOX
+%
+% Files
+%   creatCDF_USRP        - script to convert USRP sensing output to CREW common data format 
+%   createCDF_Airmagnet  - script to create dummy measurements
+%   createCDF_imec       - script to convert IMEC sensing engine output to CREW common data format
+%   crewcdf_heatmap      - Display heat map spectrogram of common data format object
+%   crewcdf_imagesc      - Display spectrogram of common data format object
+%   crewcdf_imec         - Loads imec measurement source file into common data format
+%   crewcdf_integratePSD - Calculates the power in certain frequency band
+%   crewcdf_load         - Loads the device source file to the common data format.
+%   crewcdf_loaddir      - Loads all files from dirNames directories.
+%   crewcdf_rsfsv        - Loads Rohde & Schwarz trace in ASCII format into crew cdf
+%   crewcdf_rsfsv_bin    - Loads Rohde & Schwarz trace into crew cdf (.fsv)
+%   crewcdf_savefig      - Save figure f to fig,pdf,png under 'name'
+%   crewcdf_struct       - Returns the dummy common data format structure
+%   crewcdf_telos        - Loads Telos measurement source file into common data format
+%   crewcdf_toolbox_load - CREW Common Data Format toolbox, set paths
+%   crewcdf_unifyTime    - Sets the same Tstart for all p{:} measurements
+%   crewcdf_vesna        - Returns the dummy common data format structure
+%   crewcdf_wispy        - Loads WiSpy measurement source file into common data format
+%   objdiff              - compares two objects & returns an object of the same type with just the different fields/values.
